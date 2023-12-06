4f4ae36350c7de72ca39b889e15dbdf50b8058a3
Academic34 <mixa51922915@gmail.com>
2023-12-06 12:15:14 +0300

    Initial commit

--- src/main/java/web/dao/UserDaoImp.java
+++ src/main/java/web/dao/UserDaoImp.java
@@ -1,73 +1,47 @@
 package web.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import web.model.User;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Query;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-//    @PersistenceContext
-    @Autowired
-    private EntityManagerFactory emf;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Override
-    @SuppressWarnings("unchecked")
     public List<User> getAllUsers() {
-        EntityManager entityManager = null;
-        try {
-            entityManager = emf.createEntityManager();
-            String hql = "from User";
-            Query query = entityManager.createQuery(hql, User.class);
-            return query.getResultList();
-        }
-        finally {
-            entityManager.close();
-        }
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+        Root<User> from = criteriaQuery.from(User.class);
+        criteriaQuery.select(from);
+        return entityManager.createQuery(criteriaQuery).getResultList();
     }
 
     @Override
     public void addUser(User user) {
-        EntityManager entityManager = emf.createEntityManager();
-        entityManager.getTransaction().begin();
         entityManager.persist(user);
-        entityManager.getTransaction().commit();
-        entityManager.close();
     }
 
     @Override
     public void updateUser(User user) {
-        EntityManager entityManager = emf.createEntityManager();
-        entityManager.getTransaction().begin();
         entityManager.merge(user);
-        entityManager.getTransaction().commit();
-        entityManager.close();
     }
 
     @Override
     public void removeUser(int id) {
-        EntityManager entityManager = emf.createEntityManager();
-
-        entityManager.getTransaction().begin();
         entityManager.remove(entityManager.find(User.class, id));
-        entityManager.getTransaction().commit();
-        entityManager.close();
     }
 
     @Override
     public User getUserById(int id) {
-        EntityManager entityManager = null;
-        try {
-            entityManager = emf.createEntityManager();
-            return entityManager.find(User.class, id);
-        }
-        finally {
-            entityManager.close();
-        }
+        return entityManager.find(User.class, id);
     }
 }
